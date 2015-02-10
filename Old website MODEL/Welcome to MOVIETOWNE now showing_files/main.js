c8895ae786df9366b0ae4ea706533e28f67b7f81
matthewganpat <themegatonhammer_12@hotmail.com>
2015-02-10 11:07:54 -0400

    old website at start , incase website is updated

---
+++ Old website MODEL/Welcome to MOVIETOWNE now showing_files/main.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+function mmLoadMenus() {
+  if (window.mm_menu_1106195758_0) return;
+  window.mm_menu_1106195758_0 = new Menu("root",195,18,"Verdana, Arial, Helvetica, sans-serif",12,"#0869ae","#ffffff","#ffeb00","#ff9900","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_1106195758_0.addMenuItem("The&nbsp;MovieTowne&nbsp;Family","location='family.htm'");
+   mm_menu_1106195758_0.addMenuItem("Photo&nbsp;Gallery","location='photo-gallery.htm'");
+  mm_menu_1106195758_0.addMenuItem("Contact&nbsp;Us","location='contactus.php'");
+
+  mm_menu_1106195758_0.addMenuItem("Advertise&nbsp;with&nbsp;Us","location='media_kit.htm'");
+  mm_menu_1106195758_0.addMenuItem("Theater&nbsp;Rentals","location='theater_rentals.htm'");
+  mm_menu_1106195758_0.addMenuItem("Job&nbsp;Opportunites","location='jobs.htm'");
+  mm_menu_1106195758_0.addMenuItem("Policies","location='policies.htm'");
+    mm_menu_1106195758_0.addMenuItem("Tenancy&nbsp;Application&nbsp;Form","window.open('documents/2005_Tenancy_Application_Form.pdf', '_blank');");
+	mm_menu_1106195758_0.fontWeight="bold";
+   mm_menu_1106195758_0.hideOnMouseOut=true;
+   mm_menu_1106195758_0.menuBorder=1;
+   mm_menu_1106195758_0.menuLiteBgColor='#ffffff';
+   mm_menu_1106195758_0.menuBorderBgColor='#000000';
+   mm_menu_1106195758_0.bgColor='#555555';
+  window.mm_menu_1106203038_1 = new Menu("root",95,18,"Verdana, Arial, Helvetica, sans-serif",12,"#0869ae","#ffffff","#ffeb00","#ff9900","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_1106203038_1.addMenuItem("Retail&nbsp;Stores","location='retail_stores.htm'");
+  mm_menu_1106203038_1.addMenuItem("Restaurants","location='restaurants.htm'");
+   mm_menu_1106203038_1.fontWeight="bold";
+   mm_menu_1106203038_1.hideOnMouseOut=true;
+   mm_menu_1106203038_1.menuBorder=1;
+   mm_menu_1106203038_1.menuLiteBgColor='#ffffff';
+   mm_menu_1106203038_1.menuBorderBgColor='#555555';
+   mm_menu_1106203038_1.bgColor='#555555';
+  window.mm_menu_1106204416_2 = new Menu("root",119,18,"Verdana, Arial, Helvetica, sans-serif",12,"#0869ae","#ffffff","#ffeb00","#ff9900","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_1106204416_2.addMenuItem("Birthday&nbsp;Parties","location='playrooms.htm'");
+  mm_menu_1106204416_2.addMenuItem("Video&nbsp;Arcade","location='arcade.htm'");//  mm_menu_1106204416_2.addMenuItem("Child&nbsp;Care&nbsp;Services","location='baby_sitting.htm'");
+   mm_menu_1106204416_2.fontWeight="bold";
+   mm_menu_1106204416_2.hideOnMouseOut=true;
+   mm_menu_1106204416_2.menuBorder=1;
+   mm_menu_1106204416_2.menuLiteBgColor='#ffffff';
+   mm_menu_1106204416_2.menuBorderBgColor='#555555';
+   mm_menu_1106204416_2.bgColor='#555555';
+
+    window.mm_menu_1106204419_0 = new Menu("root",100,18,"Verdana, Arial, Helvetica, sans-serif",12,"#0869ae","#ffffff","#ffeb00","#ff9900","left","middle",3,0,1000,-5,7,true,true,true,0,true,true);
+  mm_menu_1106204419_0.addMenuItem("Port of Spain","location='nowshowing.shtml'");
+  mm_menu_1106204419_0.addMenuItem("Chaguanas","location='nowshowing-chaguanas.shtml'");
+  mm_menu_1106204419_0.addMenuItem("Tobago","location='nowshowing-tobago.shtml'");
+   mm_menu_1106204419_0.fontWeight="bold";
+   mm_menu_1106204419_0.hideOnMouseOut=true;
+   mm_menu_1106204419_0.bgColor='#555555';
+   mm_menu_1106204419_0.menuBorder=1;
+   mm_menu_1106204419_0.menuLiteBgColor='#ffffff';
+   mm_menu_1106204419_0.menuBorderBgColor='#555555';
+
+mm_menu_1106204419_0.writeMenus();
+} // mmLoadMenus()
+
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
